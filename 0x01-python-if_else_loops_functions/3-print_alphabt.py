a3d449c241f6be7e6ebed4a21a95a02cb78e4e68
Ndubuisi Ollawa <ndollawa@yahoo.com>
2024-01-04 21:56:21 +0000

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-    else:
-        print("{:s}".format(chr(i)), end="")
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
